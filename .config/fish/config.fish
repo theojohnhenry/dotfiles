aa42ef83f6dbe7eef27afdd6a3449911e9ad9394
Theo Gustavsson <theojohnhenry@gmail.com>
2025-12-25 14:24:42 +0100

    zsh out fish in xd
    carl luktar kizz

---
+++ .config/fish/config.fish
@@ -0,0 +1,11 @@
+set -gx EDITOR nvim
+set -gx VISUAL nvim
+if test -x /opt/homebrew/bin/brew
+  eval (/opt/homebrew/bin/brew shellenv)
+end
+fish_add_path -g $HOME/bin
+set -g fish_greeting
+abbr -a v nvim
+abbr -a ll 'ls -la'
+abbr -a gg 'lazygit'
+abbr -a gs 'git status'
