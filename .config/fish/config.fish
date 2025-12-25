3b00dda3d0fe945cd2a099438a9ab06c34db9994
Theo Gustavsson <theojohnhenry@gmail.com>
2025-12-25 15:22:59 +0100

    rust in fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,8 @@ set -gx VISUAL nvim
 if test -x /opt/homebrew/bin/brew
   eval (/opt/homebrew/bin/brew shellenv)
 end
+# Rust (rustup)
+fish_add_path -g $HOME/.cargo/bin
 fish_add_path -g $HOME/bin
 set -g fish_greeting
 abbr -a v nvim
